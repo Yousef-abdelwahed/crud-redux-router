09f0eb0bf2898e6a1064dcec85121b29ed8da644
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2023-05-21 19:25:45 +0300

    Navbar and table
    insert Navbar and table

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,35 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div className="header">
-      <h1>CRUD APP</h1>
-      <ul className="nav">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">Add Post</a>
-        </li>
-        <li className="login">login</li>
-      </ul>
-    </div>
+    <Navbar expand="lg" bg="light">
+      <Container>
+        <Navbar.Brand href="#home">Crud</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-nav-bar">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" active={true}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#post">Add Post</Nav.Link>
+          </Nav>
+          <Nav.Link href="#login">Login</Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    // <div className="header">
+    //   <h1>CRUD APP</h1>
+    //   <ul className="nav">
+    //     <li>
+    //       <a href="/">Home</a>
+    //     </li>
+    //     <li>
+    //       <a href="/">Add Post</a>
+    //     </li>
+    //     <li className="login">login</li>
+    //   </ul>
+    // </div>
   );
 };
 
