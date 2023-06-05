f9467aca50f6b692afb48f3536176f0d40c2ba48
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2023-06-06 01:22:08 +0300

    clone Element

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
+import { cloneElement } from "react";
 import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ children }) => {
+  const newComponent = cloneElement({ children }, { title: "new Component" });
   const { isLoggedIn } = useSelector((state) => state.auth);
 
   return isLoggedIn ? children : "please login first";
