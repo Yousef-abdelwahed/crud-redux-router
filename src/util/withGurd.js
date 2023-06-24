d76f06f2fb362c374e66bd41819c848335d5c788
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2023-06-24 17:24:48 +0300

    enhance our code
    by wrap returned component and add our ? question

--- src/util/withGurd.js
+++ src/util/withGurd.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export const withGurd = (Component) => {
-  return () => {
-    return <Component />;
+  const Wrapper = (props) => {
+    const { isLoggedIn } = useSelector((state) => state.auth);
+    return isLoggedIn ? (
+      <Component {...props} />
+    ) : (
+      <div>please login first </div>
+    );
   };
+  return Wrapper;
 };
